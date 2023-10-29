b3de3b3271ed3ccd659d2463cc1e72ec125efd4d
kapoorshikhar <112186527+kapoorshikhar@users.noreply.github.com>
2023-10-29 22:46:41 +0530

    Add files via upload

---
+++ Simple Calculator/Exercise_1.py
@@ -0,0 +1,9 @@
+def Calculate(a,b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a//b)
+a=int(input("Enter the First  Number to be calculated : "))
+b=int(input("Enter the Second Number to be calculated : "))
+Calculate(a,b)
