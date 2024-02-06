67f8d0a3e24c8d3012ff6bebc04db67d5dffd05f
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-02-06 22:37:46 +0530

    Update code.py

--- KBC_game_Using_functions/code.py
+++ KBC_game_Using_functions/code.py
@@ -1,1 +1,16 @@
+import file_Exercise_3
+from file_Exercise_3 import *
+def kbc(i=0,j=0,k=0):   
+    print(f"{i+1}.{question[i]}")
+    print(f"a.{answer[k]}                b.{answer[k+1]}")
+    print(f"c.{answer[k+2]}            d.{answer[k+3]}    ")
+    user_answer=input(" Please Answer for above question : ")
+    if (user_answer==corr_answer[j]or user_answer==corr_answer[j+1] or user_answer==corr_answer[j+2]):
+        print("Answer is correct ")
+        print(f" You have Won'{money[i]}'")
+        kbc(i+1,j+3,k+4)
+    else:
+        print ("wrong Answer")
+        print(f" You have Won'{money[i-1]}'")
 
+kbc()
